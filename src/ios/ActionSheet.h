a200d4c955a57f21c90c72196ca6c2cc9baa547a
Shanth <shanth@f22labs.com>
2016-11-10 16:36:13 +0530

    fix crash on dismiss

--- src/ios/ActionSheet.h
+++ src/ios/ActionSheet.h
@@ -5,7 +5,8 @@
 @property (nonatomic, copy) NSString *callbackId;
 // TODO remove one day, as it's deprecated since iOS 8
 @property (nonatomic, retain) UIActionSheet *actionSheet;
-@property (nonatomic, retain) UIAlertController *alertController;
+// make alertController local instance and don't dealloc explictly.
+//@property (nonatomic, retain) UIAlertController *alertController;
 
 - (void) show:(CDVInvokedUrlCommand*)command;
 - (void) hide:(CDVInvokedUrlCommand*)command;
